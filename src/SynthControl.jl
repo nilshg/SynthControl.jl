2d83c34a491b0f77560081825212f376417bfd45
Nils <nilshg@hotmail.com>
2019-04-11 17:49:15 +0200

    Create working module

--- src/SynthControl.jl
+++ src/SynthControl.jl
@@ -1,101 +1,105 @@
 # To consider
-# * Multilple treatment units
+# * Multiple treatment units
 # * Exclude certain unit from comparator set
 # * StatsModels API
-
-#To delete - test data from CER Brexit study
-df = CSV.read("C:/Users/LONNG9/Desktop/Input data - cost of Brexit 2018q3.csv")
-
 module SynthControl
 
-using CSV, DataFrames, Optim, LinearAlgebra, Plots
+using DataFrames, Optim, LinearAlgebra, Plots
 
+export fit!, plot_estimates, SynthControlModel
 
 mutable struct SynthControlModel
     data::DataFrame
-    pid::Symbol
-    tid::Symbol
-    outcome::Symbol
-    treat_id
-    treat_t
+    pid::Symbol # group identifier
+    tid::Symbol # time identifier
+    outcome::Symbol # variable of interest identifier
+    treat_id # identifier for treated observations
+    treat_t # first period of treatment
+    no_treatment_t::Int64
+    no_pretreatment_t::Int64
+    no_comps::Int64
     y::Vector{Float64} # outcome observations
-    ŷ::Vector{Float64} #
-    w::Vector{Float64}
-    p_test_rest::Array{Float64, 2}
+    ŷ::Vector{Float64} # predictions
+    w::Vector{Float64} # weights
+    p_test::Array{Float64, 2} # placebo tests
 end
 
-
-
-isfitted(m::SynthControlModel) = sum(m.w) ≈ 0.0 ? false : true
+isfitted(s::SynthControlModel) = sum(s.w) ≈ 0.0 ? false : true
 
 SynthControlModel(df::DataFrame, pid::Symbol, tid::Symbol, outcome::Symbol,
                     treat_id, treat_t) = SynthControlModel(df::DataFrame,
-    pid::Symbol, tid::Symbol, outcome::Symbol, treat_id, treat_t,
-    df[df[pid] .== treat_id, outcome], zeros(length(unique(df[df[tid].>=treat_t,tid]))),
-    zeros(length(unique(df[.!(df[pid].==treat_id),pid]))))
+                    pid::Symbol,
+                    tid::Symbol,
+                    outcome::Symbol,
+                    treat_id,
+                    treat_t,
+                    0,
+                    0,
+                    0,
+                    df[df[pid] .== treat_id, outcome], # y - vector of outcomes
+                    zeros(length(unique(df[df[tid].>=treat_t,tid]))), # ŷ - predictions
+                    zeros(length(unique(df[.!(df[pid] .== treat_id), pid]))), # w - weights
+                    zeros(length(unique(df[df[tid] .>= treat_t, tid])),
+                          length(unique(df[.!(df[pid] .== treat_id), pid])))
+                    )
 
+function get_preds_weights(s::SynthControlModel; placebo = "")
 
-s = SynthControlModel(df, :country, :dateid, :realgdp, "United Kingdom", 86)
+    treat_id = isempty(placebo) ? s.treat_id : placebo
 
-function fit!(s::SynthControlModel)
     df = s.data
 
     # Get dimensions of problem
-    no_comps = length(unique(df[s.pid])) - 1
-    no_pretreat_t = length(unique(df[df[s.tid] .< s.treat_t, s.tid]))
-    no_treat_t = length(unique(df[df[s.tid] .>= s.treat_t, s.tid]))
-
-    # Estimation target
-    y_pre = s.y[1:no_pretreat_t]
+    s.no_pretreatment_t = length(unique(df[df[s.tid] .< s.treat_t, s.tid]))
+    s.no_treatment_t, s.no_comps = size(s.p_test)
 
     # Create comparator matrix
-    comps = df[.!(df[s.pid] .== s.treat_id) .& (df[s.tid] .< s.treat_t), s.outcome]
-    comps = collect(reshape(comps, no_comps, no_pretreat_t)')
+    comps = df[.!(df[s.pid] .== treat_id) .& (df[s.tid] .< s.treat_t), s.outcome]
+    comps = collect(reshape(comps, s.no_comps, s.no_pretreatment_t)')
+
+    # Pre treatment outcomes
+    y₀ = s.y[1:s.no_pretreatment_t]
 
     # Objective function
-    obj(w) = norm(y_pre - sum((w .* comps')', dims=2)) + 1_000_000*abs(1-sum(w))
+    obj(w) = norm(y₀ - sum((w .* comps')', dims = 2)) + 1_000_000*abs(1-sum(w))
 
     # Initial condition and bounds
-    initial = [1. / no_comps for _ in 1:no_comps]
-    lower = zeros(no_comps)
-    upper = ones(no_comps)
+    initial = [0.9 for _ in 1:s.no_comps]
+    lower = zeros(s.no_comps)
+    upper = ones(s.no_comps)
 
     # Get weights through optimization
-    res = optimize(obj, lower, upper, initial)
-    s.w = res.minimizer
+    res = optimize(obj, lower, upper, zeros(s.no_comps))
+    w = res.minimizer
 
-    s.ŷ = vec(sum(s.w .* reshape(df[.!(df[s.pid] .== s.treat_id), s.outcome],
-                    no_comps, length(unique(df[s.tid]))), dims = 1))
+    # Get predictions
+    ŷ = vec(sum((w .* reshape(df[.!(df[s.pid] .== treat_id), s.outcome],
+                    s.no_comps, length(unique(df[s.tid]))))', dims = 2))
+
+    return w, ŷ
 end
 
-function estimate_model(s::SynthControlModel)
-    df::DataFrame, pid::Symbol, tid::Symbol, outcome::Symbol,
-    treat_id, treat_t)
+function fit!(s::SynthControlModel; placebo_test = true)
 
-    fit!(s)
+    s.w, s.ŷ = get_preds_weights(s)
 
-    # Standard Errors
-    placebos = unique(df[.!(df[pid] .== treat_id), pid])
-    p_test_res = Array{Float64}(undef, length(placebos), no_treat_t)
-    for (i, placebo) ∈ enumerate(placebos)
-        p_test_res[i, :] = fit(df, pid, tid, outcome, placebo, t_treat)[1]
+    if placebo_test
+        # Standard Errors
+        placebos = unique(df[.!(df[s.pid] .== s.treat_id), s.pid])
+        for (i, p) ∈ enumerate(placebos)
+            ŷ = get_preds_weights(s, placebo = p)[2]
+            s.p_test[:, i] = ŷ[s.no_pretreatment_t+1:end]
+        end
     end
 
-    return δ, ŷ, w, p_test_res
+    s
 end
 
-δ, ŷ, w, placebos = estimate_model(df, pid, tid, outcome, "United Kingdom", 86)
-
-function plot_estimates()
-    plot(y, label = treat_id, legend = :topleft)
-    plot!(ŷ, label = "Control")
-    vline!([no_pretreat_t], label = "Treatment")
+function plot_estimates(s::SynthControlModel)
+    plot(s.y, label = s.treat_id, legend = :topleft)
+    plot!(s.ŷ, label = "Control")
+    vline!([s.no_pretreatment_t], label = "Treatment period")
 end
+
 
 end # module
-
-isfitted(s)
-
-fit!(s)
-
-isfitted(s)
