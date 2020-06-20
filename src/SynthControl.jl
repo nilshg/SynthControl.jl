dc44bf149f02412163d6cf090fc49c44ee14e683
Tristan Kindig <tristan.kindig@gmail.com>
2020-06-20 17:13:09 -0500

    added descriptions of fields and changed .!( .== ) syntax to .!= syntax for bit vector negations

--- src/SynthControl.jl
+++ src/SynthControl.jl
@@ -6,20 +6,37 @@ import CSV
 export SynthControlModel, fit!, isfitted, load_brexit
 
 mutable struct SynthControlModel
+    # complete dataframe
     data::DataFrame
+    # column with region names
     pid::Symbol
+    # column with integer timesteps
     tid::Symbol
+    # column with response variable
     outcome::Symbol
+    # treatment timestep (a value in tid)
+    # the treatment timestep will be considered "post-treatment"
     treat_t
+    # treated region (a value in pid)
     treat_id
+    # response vector for the treated region
     y::Vector{Float64}
+    # number of untreated regions
     no_comps::Int
+    # number of pre-treatment timesteps (excludes treatment timestep)
     no_pretreat_t::Int
+    # number of post-treatment timesteps (includes treatment timestep)
     no_treat_t::Int
+    # matrix of pre-treatment response values for untreated regions,
+    # where each row is a timestep and each column is a region
     comps::Array{Float64, 2}
+    # a vector of weights for each untreated region
     w::Vector{Float64}
+    # a vector of predictions for y
     ŷ::Vector{Float64}
+    # a vector of impact estimates for each post-treatment timestep
     δ::Vector{Float64}
+    # for placebo test
     p_test_res::Array{Float64, 2}
 end
 
@@ -27,13 +44,14 @@ function SynthControlModel(data::DataFrame, pid::Symbol, tid::Symbol, outcome::S
     treat_t, treat_id)
 
     y = data[data[!, pid] .== treat_id, outcome]
-    no_comps = length(unique(data[! ,pid])) - 1
-    no_pretreat_t = length(unique(data[data[! ,tid] .< treat_t, tid]))
-    no_treat_t = length(unique(data[data[! ,tid] .>= treat_t, tid]))
-    comp_outcomes = data[.!(data[!,pid] .== treat_id) .&( data[!,tid].<treat_t), outcome]
+    no_comps = length(unique(data[!, pid])) - 1
+    no_pretreat_t = length(unique(data[data[!, tid] .< treat_t, tid]))
+    no_treat_t = length(unique(data[data[!, tid] .>= treat_t, tid]))
+    # vector of pre-treatment response values for untreated regions
+    comp_outcomes = data[(data[!, pid] .!= treat_id) .& (data[!, tid] .< treat_t), outcome]
     comps = collect(reshape(comp_outcomes, no_comps, no_pretreat_t)')
-    ŷ = zeros(length(y))
     w = zeros(no_comps)
+    ŷ = zeros(length(y))
     δ = zeros(no_treat_t)
     p_test_res = zeros(length(y), no_comps)
 
@@ -61,13 +79,13 @@ function fit!(s::SynthControlModel; placebo_test = false)
     s.w = res.minimizer
 
     # Get estimates
-    s.ŷ = vec(sum((s.w .* reshape(s.data[.!(s.data[!,s.pid] .== s.treat_id), s.outcome],
+    s.ŷ = vec(sum((s.w .* reshape(s.data[(s.data[!,s.pid] .!= s.treat_id), s.outcome],
                     s.no_comps, length(unique(s.data[!,s.tid]))))', dims = 2))
 
     s.δ = (s.y .- s.ŷ)[s.no_pretreat_t+1:end]
 
     if placebo_test
-        placebos = unique(s.data[.!(s.data[!,s.pid] .== s.treat_id), s.pid])
+        placebos = unique(s.data[(s.data[!,s.pid] .!= s.treat_id), s.pid])
         p = deepcopy(s)
 
         for n ∈ 1:p.no_comps
@@ -75,7 +93,7 @@ function fit!(s::SynthControlModel; placebo_test = false)
             p.treat_id = placebos[n]
             # change outcome data
             p.y = p.data[p.data[!, p.pid] .== p.treat_id, p.outcome]
-            p.comps = collect(reshape(p.data[.!(p.data[!,p.pid] .== p.treat_id) .&( p.data[!,p.tid].<p.treat_t), p.outcome],
+            p.comps = collect(reshape(p.data[(p.data[!,p.pid] .!= p.treat_id) .& (p.data[!,p.tid].<p.treat_t), p.outcome],
                     p.no_comps, p.no_pretreat_t)')
             fit!(p)
             s.p_test_res[:, n] = p.y .- p.ŷ
